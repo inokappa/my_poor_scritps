087dc9efcf96ccd6c747b22e0f8d4f505820d1a8
kappa <inokara@gmail.com>
2013-11-15 06:19:11 +0900

    first commit

---
+++ put_data2es.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+_index=logstash-YYYY.MM.DD
+_type=memo
+logdate=`date +%Y-%m-%d`
+
+i=1
+while [ $i -le 100 ];
+do
+  echo "$i"
+  i=`expr $i + 1`
+  timestamp=`date +%H:%M:%S`
+  sleep 1
+  curl -XPUT "http://localhost:9200/$_index/$_type/$i" -d \
+  '{"name" : "kappa","date" : '$logdate',"message" : "test" ,"v" : '$i' ,"@log_name" : "test", "@timestamp" : "'$logdate'T'$timestamp'+09:00"}'
+done
